7c23beb72d3b69744f66236db3f05796ee01b581
AnchalVerma-Creditas <100260971+AnchalVerma-Creditas@users.noreply.github.com>
2025-11-17 13:31:11 +0530

    feat: update button component

--- components/button/index.js
+++ components/button/index.js
@@ -3,7 +3,7 @@ function Button(props) {
   const { children, onClick, variant = 'primary' } = props;
   
   const buttonStyles = {
-    primary: 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded',
+    primary: 'bg-blue-500 text-[18px] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded',
     secondary: 'bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded',
   };
   
